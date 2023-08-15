079d9a387bf80ceeca22a6d0ebdd9ba387ce9877
Ogechukwu Aniobi <aniobioge@gmail.com>
2023-08-15 02:55:45 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,30 @@
 #define DOG_H
 
 /**
- * struct dog - Structure representing a dog.
- * @name: Pointer to a string representing the name of the dog.
- * @age: Floating point value representing the age of the dog.
- * @owner: Pointer to a string representing the name of the dog's owner.
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: This structure defines the properties of a dog, including
- * its name, age, and owner's name.
+ * Description: Longer description
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
